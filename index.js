584068d4dcb2dc002f448f06ccf3d5cf85e9f346
Zhumabai00 <asanaalyev1@gmail.com>
2024-01-14 00:13:30 +0700

    mvc

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import cors from 'cors'
 import mongoose from 'mongoose'
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 
@@ -24,6 +25,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 app.use(express.json())
+app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
 app.get('/', (req, res) => {
@@ -37,6 +39,7 @@ app.get('/auth/me', checkAuth, UserController.getMe)
 
 
 app.get('/posts', PostController.getAll)
+app.get('/tags', PostController.getLastTags)
 app.get('/posts/:id', PostController.getOne)
 
 app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
